c159e24070a11cad64980ff1dfd41ced84e2eec1
Munish230398 <159116305+Munish230398@users.noreply.github.com>
2024-08-06 15:01:13 +0530

    Update __init__.py

--- prediction_model/employee_attrition/__init__.py
+++ prediction_model/employee_attrition/__init__.py
@@ -1,4 +1,4 @@
-# import os
-# from employee_attrition.config import config 
-# with open(os.path.join(config.PACKAGE_ROOT,'VERSION')) as f:
-#     __version__ = f.read().strip()
+import os
+from employee_attrition.config import config 
+with open(os.path.join(config.PACKAGE_ROOT,'VERSION')) as f:
+    __version__ = f.read().strip()
